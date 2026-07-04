a191166b85724e461fa0dcc690636e3770229c4f
Lochie <95149116+Lochi-dot-JPEG@users.noreply.github.com>
2026-07-05 09:10:27 +1000

    feat: block shapes and rotation

--- entities/tetromino/tetromino.gd
+++ entities/tetromino/tetromino.gd
@@ -20,6 +20,83 @@ const TYPE_COLORS = [
 	Color("#a8ca85")
 	]
 
+const BLOCK_POSITIONS = [
+	# Line
+	[
+		[ # Unrotated
+			[0,0], [-1,0], [1,0], [2,0] ],
+		[ # 90degrees
+			[1,0], [1,1], [1,-1], [1,2] ],
+		[ # 180degrees
+			[0,1], [-1,1], [1,1], [2,1] ],
+		[ # 270degrees
+			[0,0], [0,1], [0,-1], [0,2] ],
+	],
+
+	[ # Square
+		[ # Unrotated
+			[0,0], [1,0], [1,1], [0,1] ],
+		[ # 90degrees
+			[0,0], [1,0], [1,1], [0,1] ],
+		[ # 180degrees
+			[0,0], [1,0], [1,1], [0,1] ],
+		[ # 270degrees
+			[0,0], [1,0], [1,1], [0,1] ],
+	],
+	[ # L
+		[ # Unrotated
+			[0,0], [1,0], [-1,0], [1,-1] ],
+		[ # 90degrees
+			[0,0], [0,1], [1,1], [0,-1] ],
+		[ # 180degrees
+			[0,0], [1,0], [-1,0], [-1,1] ],
+		[ # 270degrees
+			[0,0], [0,1], [-1,-1], [0,-1] ],
+	],
+	[ # Reverse L
+		[ # Unrotated
+			[0,0], [1,0], [-1,0], [-1,-1] ],
+		[ # 90degrees
+			[0,0], [0,1], [1,-1], [0,-1] ],
+		[ # 180degrees
+			[0,0], [1,0], [-1,0], [1,1] ],
+		[ # 270degrees
+			[0,0], [0,1], [0,-1], [-1,1] ],
+	],
+	[ # T
+		[ # Unrotated
+			[0,0], [1,0], [-1,0], [0,-1] ],
+		[ # 90degrees
+			[0,0], [1,0],  [0,-1], [0,1] ],
+		[ # 180degrees
+			[0,0], [1,0], [-1,0],  [0,1] ],
+		[ # 270degrees
+			[0,0], [-1,0], [0,-1], [0,1] ],
+	],
+	[ # Z
+		[ # Unrotated
+			[0,0], [1,0], [0,-1], [-1,-1] ],
+		[ # 90degrees
+			[0,0], [1,0],  [0,1], [1,-1] ],
+		[ # 180degrees
+			[0,1], [1,1], [0,0], [-1,0] ],
+		[ # 270degrees
+			[0,0], [-1,0], [0,-1], [-1,1] ],
+	],
+	[ # S
+		[ # Unrotated
+			[0,0], [-1,0], [0,-1], [1,-1] ],
+		[ # 90degrees
+			[0,0], [1,0],  [1,-1], [0,1] ],
+		[ # 180degrees
+			[0,0], [-1,1], [1,0],  [0,1] ],
+		[ # 270degrees
+			[-1,0], [0,0],  [0,1], [-1,-1] ],
+	],
+
+
+	]
+
 @onready var blocks : Array[Sprite2D] = [ 
 	get_node("%block"),
 	get_node("%block2"),
@@ -27,15 +104,34 @@ const TYPE_COLORS = [
 	get_node("%block4"),
 ]
 
+var piece_rotation := 0
+var type := 0
+
 func _ready() -> void:
 	pass
 
-func _load_block(type: int) -> void:
+func _load_block(_type: int) -> void:
+	type = _type
 	for block in blocks:
 		block.modulate = TYPE_COLORS[type]
+	piece_rotation = 0
+	_load_rotation()
+
+func _load_rotation() -> void:
+	for i in 4:
+		var pos = BLOCK_POSITIONS[type][piece_rotation][i]
+		blocks[i].position.x = pos[0] * 32
+		blocks[i].position.y = pos[1] * 32
 
 func _process(delta: float) -> void:
 	if OS.is_debug_build():
+		if Input.is_action_just_pressed("rotate"):
+			piece_rotation += 1 
+			while piece_rotation < 0:
+				piece_rotation += 4
+			while piece_rotation > 3:
+				piece_rotation -= 4
+			_load_rotation()
 		if Input.is_key_pressed(KEY_1):
 			_load_block(1)
 		if Input.is_key_pressed(KEY_2):
